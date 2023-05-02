a41c0c73843bc7d09dcd3b1cfc3b396fce656e59
Masynchin <masynchin@gmail.com>
2023-05-02 15:40:23 +0000

    Deploying to gh-pages from @ Masynchin/LogicPapers@c65e1614f17691d0c48718ea12cf23daf0f74fc2 🚀

--- assets/PresenterPrint-6b251cda.js
+++ assets/PresenterPrint-6b251cda.js
@@ -1,4 +1,4 @@
-import{d as _,i as d,a as p,u as h,b as u,c as m,e as f,f as n,g as t,t as s,h as a,F as g,r as v,n as x,j as y,o as r,k as b,l as k,m as N,p as P,q as w,_ as L}from"./index-f5d7cb27.js";import{N as S}from"./NoteDisplay-97f69068.js";const V={class:"m-4"},j={class:"mb-10"},T={class:"text-4xl font-bold mt-2"},B={class:"opacity-50"},C={class:"text-lg"},D={class:"font-bold flex gap-2"},H={class:"opacity-50"},z=t("div",{class:"flex-auto"},null,-1),F={key:0,class:"border-gray-400/50 mb-8"},M=_({__name:"PresenterPrint",setup(q){d(p),h(`
+import{d as _,i as d,a as p,u as h,b as u,c as m,e as f,f as n,g as t,t as s,h as a,F as g,r as v,n as x,j as y,o as r,k as b,l as k,m as N,p as P,q as w,_ as L}from"./index-f6b609b1.js";import{N as S}from"./NoteDisplay-1745dfa5.js";const V={class:"m-4"},j={class:"mb-10"},T={class:"text-4xl font-bold mt-2"},B={class:"opacity-50"},C={class:"text-lg"},D={class:"font-bold flex gap-2"},H={class:"opacity-50"},z=t("div",{class:"flex-auto"},null,-1),F={key:0,class:"border-gray-400/50 mb-8"},M=_({__name:"PresenterPrint",setup(q){d(p),h(`
 @page {
   size: A4;
   margin-top: 1.5cm;
